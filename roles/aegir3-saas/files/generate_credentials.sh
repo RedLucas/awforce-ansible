1159e2e57f3db6e3dc7f3d30a6ea84e5b973b022
Guillaume Boudrias <gboudrias@praxis.coop>
2016-04-23 04:27:08 +0000

    Add aegir-saas-hub role and generate aggregated server file

--- roles/aegir3-saas/files/generate_credentials.sh
+++ roles/aegir3-saas/files/generate_credentials.sh
@@ -4,13 +4,11 @@ API_KEY=`drush @hostmaster vget --format=string --exact hosting_saas_api_key`
 OAUTH_KEY=`drush @hostmaster hnn-default key`
 OAUTH_SECRET=`drush @hostmaster hnn-default secret`
 
-OUTPUT='"'$1'": {'$'\n'
-OUTPUT+=$'  "type": "hostmaster_saas",\n'
-OUTPUT+=$'  "hostmaster_saas": {\n'
-OUTPUT+=$'    "port":"0",\n'
-OUTPUT+=$'    "oauth_key":"'$OAUTH_KEY$'",\n'
-OUTPUT+=$'    "oauth_secret":"'$OAUTH_SECRET$'",\n'
-OUTPUT+=$'    "saas_api_key":"'$API_KEY$'",\n'
-OUTPUT+=$'  }\n'
-OUTPUT+=$'}  \n'
-echo "$OUTPUT"
+OUTPUT+=$'    "type": "hostmaster_saas",\n'
+OUTPUT+=$'    "hostmaster_saas": {\n'
+OUTPUT+=$'      "port":"0",\n'
+OUTPUT+=$'      "oauth_key":"'$OAUTH_KEY$'",\n'
+OUTPUT+=$'      "oauth_secret":"'$OAUTH_SECRET$'",\n'
+OUTPUT+=$'      "saas_api_key":"'$API_KEY$'"\n'
+OUTPUT+=$'    }'
+printf "%s" "$OUTPUT"
