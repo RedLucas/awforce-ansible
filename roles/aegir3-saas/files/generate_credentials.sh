2263fecee7cded249cbd4b6977dd77053a70f2a8
root <root@ip-10-0-0-188.us-west-2.compute.internal>
2016-08-18 17:29:03 +0000

    fix saas key

--- roles/aegir3-saas/files/generate_credentials.sh
+++ roles/aegir3-saas/files/generate_credentials.sh
@@ -1,6 +1,6 @@
 
 
-API_KEY=`drush @hostmaster vget --format=string --exact hosting_saas_api_key`
+API_KEY=`(drush @hostmaster ctev services_endpoint hosts && echo "echo \$endpoint->authentication['services_api_key_auth']['api_key'];")|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"|php -a|sed '3q;d'`
 OAUTH_KEY=`drush @hostmaster hnn-default key`
 OAUTH_SECRET=`drush @hostmaster hnn-default secret`
 
