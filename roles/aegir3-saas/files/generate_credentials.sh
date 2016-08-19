5dcb1adddf484eda9d3649065903f932a48996d2
James Kennedy <j@iamjameskennedy.com>
2016-08-18 22:12:59 -0700

    many fixes

--- roles/aegir3-saas/files/generate_credentials.sh
+++ roles/aegir3-saas/files/generate_credentials.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 
-
-API_KEY=`(drush @hostmaster ctev services_endpoint hosts && echo "echo \$endpoint->authentication['services_api_key_auth']['api_key'];")|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"|php -a|sed '3q;d'`
+API_KEY=`(drush @hostmaster ctev services_endpoint hosting_saas && echo "echo \\$endpoint->authentication['services_api_key_auth']['api_key'];")|sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"|php -a|sed '3q;d'`
 OAUTH_KEY=`drush @hostmaster hnn-default key`
 OAUTH_SECRET=`drush @hostmaster hnn-default secret`
 
-OUTPUT+=$'    "type": "hostmaster_saas",\n'
-OUTPUT+=$'    "hostmaster_saas": {\n'
-OUTPUT+=$'      "port":"0",\n'
-OUTPUT+=$'      "oauth_key":"'$OAUTH_KEY$'",\n'
-OUTPUT+=$'      "oauth_secret":"'$OAUTH_SECRET$'",\n'
-OUTPUT+=$'      "saas_api_key":"'$API_KEY$'"\n'
-OUTPUT+=$'    }'
+OUTPUT=''
+OUTPUT+=$'"type": "hostmaster_saas",\n'
+OUTPUT+=$'"hostmaster_saas": {\n'
+OUTPUT+=$'  "port":"0",\n'
+OUTPUT+=$'  "oauth_key":"'$OAUTH_KEY$'",\n'
+OUTPUT+=$'  "oauth_secret":"'$OAUTH_SECRET$'",\n'
+OUTPUT+=$'  "saas_api_key":"'$API_KEY$'"\n'
+OUTPUT+=$'}'
 printf "%s" "$OUTPUT"
